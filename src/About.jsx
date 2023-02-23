407d556f21e613efaaf1f886e2fb25150800ee46
francwow <francwow06@gmail.com>
2023-02-23 18:11:45 -0500

    add changes to About component

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,18 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
 import MenuActiveContext from "./contexts/menuActiveContext";
 import Icons from "./Icons";
 import Footer from "./Footer";
 
 const About = () => {
   const menuActive = useContext(MenuActiveContext);
+  const { ref: profileRef, inView: profileInView } = useInView({
+    threshold: 0.3,
+  });
+  const { ref: deskRef, inView: deskInView } = useInView({
+    threshold: 0.3,
+  });
 
   return (
     <div
@@ -18,28 +25,57 @@ const About = () => {
               <h2>Hello World!</h2>
             </div>
             <div className="about-intro-description">
-              <div className="desc-overlay"></div>
               <p>
                 My name is Francisco Osorio Silva. Im an adventure seeker,
                 animal lover, running enthusiast and avid gamer, who enjoys
                 learning and overcoming challenges.
                 <br></br>
                 <br></br>
-                I&apos;ve been learning front-end development for the last 12
-                months. I love everything about this craft, from designing the
-                layout of a website and picking the right color composition, to
-                writing DRY Javascript and meaningful CSS classes. I can&apos;t
-                get enough of it. It&apos;s been a fantastic journey so far.
-                <br></br>
-                <br></br>
-                I&apos;ve had the opportunity of working on freelance projects
-                for real clients, and playing with fun personal ideas. You can
-                find all my work <Link to="/work">here</Link>.
               </p>
             </div>
           </div>
-          <div className="about-intro-img">
+          <div ref={profileRef} className="about-intro-img">
+            <div
+              className={
+                profileInView ? "desc-overlay imgShow" : "desc-overlay"
+              }
+            ></div>
             <img src="src/assets/profilepic.webp" alt="Francisco smiling" />
+          </div>
+        </div>
+        <div className="about-hobbies">
+          <div className="hobbies-info">
+            <div className="hobbies-header">
+              <h2>Other stuff I&apos;m into</h2>
+            </div>
+            <div className="about-intro-description">
+              <p>
+                I love to go for long runs in the morning right after my first
+                coffee. I enjoy reading about sicence, psychology and human
+                interactions. People are very important to me, I like to visit
+                friends and family whenever I can.
+              </p>
+            </div>
+          </div>
+          <div ref={deskRef} className="about-intro-img">
+            <div
+              className={deskInView ? "desc-overlay imgShow" : "desc-overlay"}
+            ></div>
+            <img src="src/assets/desk.webp" alt="Francisco smiling" />
+          </div>
+          <div className="about-intro-description">
+            <p>
+              I&apos;ve been learning front-end development for the last 12
+              months. I love everything about this craft, from designing the
+              layout of a website and picking the right color composition, to
+              writing DRY Javascript and meaningful CSS classes. I can&apos;t
+              get enough of it. It&apos;s been a fantastic journey so far.
+              <br></br>
+              <br></br>
+              I&apos;ve had the opportunity of working on freelance projects for
+              real clients, and playing with fun personal ideas. You can find
+              all my work <Link to="/work">here</Link>.
+            </p>
           </div>
         </div>
         <div className="footer-wrapper">
