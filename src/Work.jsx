de5d4f5f409e03d3112e66ef347b7c005ded306e
francwow <francwow06@gmail.com>
2023-03-02 17:11:13 -0500

    add carousel sttyles

--- src/Work.jsx
+++ src/Work.jsx
@@ -1,159 +1,22 @@
 import MenuActiveContext from "./contexts/menuActiveContext";
 import LanguageContext from "./contexts/languageContext";
-import { Link } from "react-router-dom";
 import { useContext, useEffect, useRef } from "react";
-import { useInView } from "react-intersection-observer";
 import { useMediaQuery } from "usehooks-ts";
+import WorkMobile from "./WorkMobile";
+import WorkDeskTop from "./WorkDeskTop";
+import Footer from "./Footer";
+import Icons from "./Icons";
 
 const Work = () => {
   const contRef = useRef(null);
   const { menuActive } = useContext(MenuActiveContext);
   const { englishActive } = useContext(LanguageContext);
-  const { ref: item1, inView: item1InView } = useInView({
-    threshold: 0.9,
-  });
-  const { ref: item2, inView: item2InView } = useInView({
-    threshold: 0.9,
-  });
-  const { ref: item3, inView: item3InView } = useInView({
-    threshold: 0.9,
-  });
-  const { ref: item4, inView: item4InView } = useInView({
-    threshold: 0.9,
-  });
-  const { ref: item5, inView: item5InView } = useInView({
-    threshold: 0.9,
-  });
-  const { ref: item6, inView: item6InView } = useInView({
-    threshold: 0.9,
-  });
-  const { ref: item7, inView: item7InView } = useInView({
-    threshold: 0.9,
-  });
-  const matches = useMediaQuery("(max-width: 480px)");
+  const mobile = useMediaQuery("(max-width: 480px)");
 
   return (
     <div className={`main-section  ${menuActive ? "move-left" : ""}`}>
       <div ref={contRef} className="work-wrapper work-background">
-        {matches ? (
-          <div className="work-container">
-            <Link to="https://www.depositosanantonio.com/" target="_blank">
-              <div
-                ref={item1}
-                className={item1InView ? "work-item item-expand" : "work-item"}
-              >
-                <img
-                  className={item1InView ? "img-expand" : null}
-                  src="/deposito.webp"
-                  alt="website from portfolio"
-                />
-              </div>
-            </Link>
-            <Link to="https://www.depositosanantonio.com/" target="_blank">
-              <div
-                ref={item2}
-                className={item2InView ? "work-item item-expand" : "work-item"}
-              >
-                <img
-                  className={item2InView ? "img-expand" : null}
-                  src="/latiendo.webp"
-                  alt="website from portfolio"
-                />
-              </div>
-            </Link>
-            <Link to="https://www.depositosanantonio.com/" target="_blank">
-              <div
-                ref={item3}
-                className={item3InView ? "work-item item-expand" : "work-item"}
-              >
-                <img
-                  className={item3InView ? "img-expand" : null}
-                  src="/flagFinder.webp"
-                  alt="website from portfolio"
-                />
-              </div>
-            </Link>
-            <Link to="https://www.depositosanantonio.com/" target="_blank">
-              <div
-                ref={item4}
-                className={item4InView ? "work-item item-expand" : "work-item"}
-              >
-                <img
-                  className={item4InView ? "img-expand" : null}
-                  src="/eloquentjs.webp"
-                  alt="website from portfolio"
-                />
-              </div>
-            </Link>
-            <Link to="https://www.depositosanantonio.com/" target="_blank">
-              <div
-                ref={item5}
-                className={item5InView ? "work-item item-expand" : "work-item"}
-              >
-                <img
-                  className={item5InView ? "img-expand" : null}
-                  src="/franos.webp"
-                  alt="website from portfolio"
-                />
-              </div>
-            </Link>
-            <Link to="https://www.depositosanantonio.com/" target="_blank">
-              <div
-                ref={item6}
-                className={item6InView ? "work-item item-expand" : "work-item"}
-              >
-                <img
-                  className={item6InView ? "img-expand" : null}
-                  src="/arthas.webp"
-                  alt="website from portfolio"
-                />
-              </div>
-            </Link>
-            <Link to="https://www.depositosanantonio.com/" target="_blank">
-              <div
-                ref={item7}
-                className={item7InView ? "work-item item-expand" : "work-item"}
-              >
-                <img
-                  className={item7InView ? "img-expand" : null}
-                  src="/fos.webp"
-                  alt="website from portfolio"
-                />
-              </div>
-            </Link>
-          </div>
-        ) : (
-          <div ref={contRef} className="work-container">
-            <div
-              ref={item1}
-              className={item1InView ? "work-item item-expand" : "work-item"}
-            ></div>
-            <div
-              ref={item2}
-              className={item2InView ? "work-item item-expand" : "work-item"}
-            ></div>
-            <div
-              ref={item3}
-              className={item3InView ? "work-item item-expand" : "work-item"}
-            ></div>
-            <div
-              ref={item4}
-              className={item4InView ? "work-item item-expand" : "work-item"}
-            ></div>
-            <div
-              ref={item5}
-              className={item5InView ? "work-item item-expand" : "work-item"}
-            ></div>
-            <div
-              ref={item6}
-              className={item6InView ? "work-item item-expand" : "work-item"}
-            ></div>
-            <div
-              ref={item7}
-              className={item7InView ? "work-item item-expand" : "work-item"}
-            ></div>
-          </div>
-        )}
+        {mobile ? <WorkMobile /> : <WorkDeskTop />}
       </div>
     </div>
   );
---
+++ src/WorkDeskTop.jsx
@@ -0,0 +1,117 @@
+import { Link } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
+
+const WorkDeskTop = () => {
+  const { ref: item1, inView: item1InView } = useInView({
+    threshold: 0.9,
+  });
+  const { ref: item2, inView: item2InView } = useInView({
+    threshold: 0.9,
+  });
+  const { ref: item3, inView: item3InView } = useInView({
+    threshold: 0.9,
+  });
+  const { ref: item4, inView: item4InView } = useInView({
+    threshold: 0.9,
+  });
+  const { ref: item5, inView: item5InView } = useInView({
+    threshold: 0.9,
+  });
+  const { ref: item6, inView: item6InView } = useInView({
+    threshold: 0.9,
+  });
+  const { ref: item7, inView: item7InView } = useInView({
+    threshold: 0.9,
+  });
+
+  return (
+    <div className="work-container">
+      <Link to="https://www.depositosanantonio.com/" target="_blank">
+        <div
+          ref={item1}
+          className={item1InView ? "work-item item-expand" : "work-item"}
+        >
+          <img
+            className={item1InView ? "img-expand" : null}
+            src="/deposito.webp"
+            alt="website from portfolio"
+          />
+        </div>
+      </Link>
+      <Link to="https://www.depositosanantonio.com/" target="_blank">
+        <div
+          ref={item2}
+          className={item2InView ? "work-item item-expand" : "work-item"}
+        >
+          <img
+            className={item2InView ? "img-expand" : null}
+            src="/latiendo.webp"
+            alt="website from portfolio"
+          />
+        </div>
+      </Link>
+      <Link to="https://www.depositosanantonio.com/" target="_blank">
+        <div
+          ref={item3}
+          className={item3InView ? "work-item item-expand" : "work-item"}
+        >
+          <img
+            className={item3InView ? "img-expand" : null}
+            src="/flagFinder.webp"
+            alt="website from portfolio"
+          />
+        </div>
+      </Link>
+      <Link to="https://www.depositosanantonio.com/" target="_blank">
+        <div
+          ref={item4}
+          className={item4InView ? "work-item item-expand" : "work-item"}
+        >
+          <img
+            className={item4InView ? "img-expand" : null}
+            src="/eloquentjs.webp"
+            alt="website from portfolio"
+          />
+        </div>
+      </Link>
+      <Link to="https://www.depositosanantonio.com/" target="_blank">
+        <div
+          ref={item5}
+          className={item5InView ? "work-item item-expand" : "work-item"}
+        >
+          <img
+            className={item5InView ? "img-expand" : null}
+            src="/franos.webp"
+            alt="website from portfolio"
+          />
+        </div>
+      </Link>
+      <Link to="https://www.depositosanantonio.com/" target="_blank">
+        <div
+          ref={item6}
+          className={item6InView ? "work-item item-expand" : "work-item"}
+        >
+          <img
+            className={item6InView ? "img-expand" : null}
+            src="/arthas.webp"
+            alt="website from portfolio"
+          />
+        </div>
+      </Link>
+      <Link to="https://www.depositosanantonio.com/" target="_blank">
+        <div
+          ref={item7}
+          className={item7InView ? "work-item item-expand" : "work-item"}
+        >
+          <img
+            className={item7InView ? "img-expand" : null}
+            src="/fos.webp"
+            alt="website from portfolio"
+          />
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default WorkDeskTop;
